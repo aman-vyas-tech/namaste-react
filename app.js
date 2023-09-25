9d4e58aef2db7ef3e0d98f12ede8983424a6d1dc
Aman Vyas <aman03040@gmail.com>
2023-09-25 22:28:08 +0530

    Episode 3: Practice

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ import ReactDOM from "react-dom/client";
 const element = React.createElement("h1", {id: "heading"}, "This is H1 tag");
 
 // JSX transpiled by Pracel(Babel inside) before reaches to JS engine.
-const jsxElement = <h1 id="heading"> JSX Heading Element</h1>
+const jsxElement = <h1 id="heading"> JSX Heading Element</h1>;
+const jsxElement1 = <h1 id="heading"> JSX Heading Element 1</h1>;
+
 const root = ReactDOM.createRoot(document.getElementById("id"));
 root.render(jsxElement);
+root.render(jsxElement1);
