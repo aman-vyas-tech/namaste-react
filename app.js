0e48cbaef45df3fe79bfffe94d54d0b1a74c599f
Aman Vyas <aman03040@gmail.com>
2023-09-29 16:54:19 +0530

    Espisode4: res card layout and rendering comps

--- app.js
+++ app.js
@@ -35,13 +35,17 @@ const Header = ()=> {
 };
 
 const styleCard = {
-    backgroundColor: "pink"
+    backgroundColor: "#f0f0f1"
 }
 
 const RestaurentCard = () => {
     return (
         <div className="res-card" style={styleCard}>
+            <img className="res-image" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/e0vvulfbahjxjz6k4uwi" />
             <h3>Radha Krishna</h3>
+            <h4>North India, Punjabi</h4>
+            <h4>4.4 stars</h4>
+            <h4>35 mins</h4>
         </div>
     )
 }
@@ -53,6 +57,15 @@ const Body = ()=> {
                 Search
             </div>
             <div className="res-container">
+                <RestaurentCard/>
+                <RestaurentCard/>
+                <RestaurentCard/>
+                <RestaurentCard/>
+                <RestaurentCard/>
+                <RestaurentCard/>
+                <RestaurentCard/>
+                <RestaurentCard/>
+                <RestaurentCard/>
                 <RestaurentCard/>
             </div>
         </div>
