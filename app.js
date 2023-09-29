d2e73d2f6891411715f8d38bfed7b66569fcdebe
Aman Vyas <aman03040@gmail.com>
2023-09-29 11:37:05 +0530

    Episode3: Component composition

--- app.js
+++ app.js
@@ -13,18 +13,26 @@ import ReactDOM from "react-dom/client";
 //   </h1>
 // );
 
-//react Func component which return jsx.
-const HeadingComponent = () => (
-<div id="container">
-    <h1 id="heading" className="head">JSX Heading Element 1</h1>
-</div>
-);
-
 const TitleComponent = () => (
     <div id="title">
         <h1 id="title">React title</h1>
     </div>
 );
 
+const Subject = () => (
+    <div id="subject">
+        <p>This ia  an article on component compisition where one component nested into anoter</p>
+    </div>
+);
+
+//React Func component which return jsx and here nested component is called component composition.
+const HeadingComponent = () => (
+<div id="container">
+    <TitleComponent/>
+    <h1 id="heading" className="head">JSX Heading Element 1</h1>
+    <Subject/>
+</div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("id"));
-root.render(<HeadingComponent />,<TitleComponent/>);
+root.render(<HeadingComponent />);
